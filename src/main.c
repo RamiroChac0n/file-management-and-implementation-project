cf95cedb71e699d15980ea00847f6d30d94f1927
Ramiro Chacón <ramiro20010202@gmail.com>
2023-11-13 19:10:49 -0600

    Add login functionality to main.c

--- src/main.c
+++ src/main.c
@@ -3,7 +3,32 @@
 #include <string.h>
 #include "database.h"
 
+void login();
+void remove_newline(char *str);
+
 int main(int argc, char *argv[]) {
     test_database_connection();
+    login();
     return 0;
+}
+
+void remove_newline(char *str) {
+    int len = strlen(str);
+    if (len > 0 && str[len-1] == '\n') {
+        str[len-1] = '\0';
+    }
+}
+
+
+void login() {
+    char *account_number = malloc(100);
+    char *password = malloc(100);
+    printf("Login\n");
+    printf("Account Number: ");
+    fgets(account_number, 100, stdin);
+    printf("Password: ");
+    fgets(password, 100, stdin);
+
+    remove_newline(account_number);
+    remove_newline(password);
 }
