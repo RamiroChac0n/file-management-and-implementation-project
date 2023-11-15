c8ac96bb66b7caa4c693339c3c2c501b5f88ed2c
Ramiro Chacón <ramiro20010202@gmail.com>
2023-11-15 00:19:03 -0600

    Add account struct and get_account function to database.c

--- src/main.c
+++ src/main.c
@@ -21,14 +21,31 @@ void remove_newline(char *str) {
 
 
 void login() {
-    char *account_number = malloc(100);
+    int id_account;
     char *password = malloc(100);
     printf("Login\n");
     printf("Account Number: ");
-    fgets(account_number, 100, stdin);
+    fflush(stdin);
+    scanf("%d", &id_account);
+
+    while (getchar() != '\n');
     printf("Password: ");
     fgets(password, 100, stdin);
 
-    remove_newline(account_number);
     remove_newline(password);
+
+    Account *account = get_account(id_account);
+
+    if(account != NULL && strcmp(account->password, password) == 0) {
+        system("clear");
+        printf("Login Successful\n\n");
+        printf("Account Number: %d\n", account->id_account);
+        printf("Password: %s\n", account->password);
+        printf("Funds: $%.2f\n", account->funds);
+        printf("Bank: %d\n", account->id_bank);
+        printf("Type: %d\n", account->id_type);
+        printf("Client: %d\n", account->id_client);        
+    } else {
+        printf("Login Failed\n");
+    }
 }
